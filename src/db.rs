f6b4f828a15757044e652f5f7de06808ff3927ba
Uxío García Andrade <uxiog21@gmail.com>
2021-02-12 20:17:51 +0100

    add db connection

---
+++ src/db.rs
@@ -0,0 +1,43 @@
+use diesel::pg::PgConnection;
+use r2d2;
+use r2d2_diesel::ConnectionManager;
+use rocket::{Outcome, Request, State};
+use rocket::http::Status;
+use rocket::request::{self, FromRequest};
+use std::env;
+use std::ops::Deref;
+
+type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+static DATABASE_URL: &str = "postgres://postgres:password@localhost/blog";
+
+pub fn init_pool() -> Pool {
+    let manager = ConnectionManager::<PgConnection>::new(DATABASE_URL);
+    Pool::new(manager).expect("db pool")
+}
+
+fn database_url() -> String {
+    env::var("DATABASE_URL").expect("DATABASE_URL must be set")
+}
+
+pub struct DbConn(pub r2d2::PooledConnection<ConnectionManager<PgConnection>>);
+
+impl<'a, 'r> FromRequest<'a, 'r> for DbConn {
+    type Error = ();
+
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<DbConn, Self::Error> {
+        let pool = request.guard::<State<Pool>>()?;
+        match pool.get() {
+            Ok(conn) => Outcome::Success(DbConn(conn)),
+            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ())),
+        }
+    }
+}
+
+impl Deref for DbConn {
+    type Target = PgConnection;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
